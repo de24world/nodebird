5685c080e01a939fcd82808843dbdd030668ca6e
Lee, Hosoo <Hosoo.Lee@arithnea.de>
2020-04-27 00:38:10 +0200

    기본 라우터와 pug 파일 세팅

--- app.js
+++ app.js
@@ -17,16 +17,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser('process.env.COOKIE_SECRET'));
-app.use(session({
+const sessionOption = {
     resave: false,
     saveUninitialized: false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
-        httpOnly: true,
-        secure: false,
+      httpOnly: true,
+      secure: false,
     },
-    app.use(flash());
-}))
+    store: new RedisStore({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      pass: process.env.REDIS_PASSWORD,
+      logErrors: true,
+    }),
+  };
+  if (process.env.NODE_ENV === 'production') {
+    sessionOption.proxy = true;
+    // sessionOption.cookie.secure = true;
+  }
+  app.use(session(sessionOption));
+  app.use(flash());
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+  app.use('/', indexRouter);
+  app.use('/user', userRouter);
+  app.use('/auth', authRouter);
+  app.use('/post', postRouter);
+  
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    logger.info('hello'); // console.info 대체
+    logger.error(err.message); // console.error 대체
+    next(err);
+  });
+  
+  app.use((err, req, res) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+  });
+  
 
 app.listen(app.get('port'), () => {
     console.log(`${app.get('port')} number Port Running Server `);
